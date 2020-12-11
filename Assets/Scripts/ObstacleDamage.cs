3991d9c1376570363c9c871b74d5f03f2041555e
Eduardo2719 <eduardoloboscabre2017@hotmail.com>
2020-12-11 18:49:29 -0300

    Initial commit

---
+++ Assets/Scripts/ObstacleDamage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDamage : MonoBehaviour
+{
+    public GameObject destuctibleObject;
+    public GameObject explosion;
+    private CarController carcontroller;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carcontroller = GameObject.FindGameObjectWithTag("BoxCollider3").GetComponent<CarController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void hurtPlayer(int damage)
+    {
+        carcontroller.vehicleHealth -= damage;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BoxCollider3")
+        {
+            
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            GameObject destrutibleBox = Instantiate(destuctibleObject, transform.position, transform.rotation) as GameObject;
+            Rigidbody[] allRigibodies = destrutibleBox.GetComponentsInChildren<Rigidbody>();
+            if (allRigibodies.Length > 0)
+            {
+                foreach(var body in allRigibodies)
+                {
+                    body.AddExplosionForce(500, transform.position, 1);
+                }
+            }
+            hurtPlayer(1);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    public float velocity;
+    public float zerovelocity = 0f;
+    public bool destruible;
+    public GameObject obstaculo;
+    private TimeManager timemanager;
+    public AudioSource source;
+    public AudioClip[] carHorn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timemanager = GameObject.FindGameObjectWithTag("TimeManager").GetComponent<TimeManager>();
+    }
+
+    void MueveObstaculo()
+    {
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("DestroyTrigger")==true && destruible == true)
+        {
+            Destroy(gameObject);
+        }
+
+        if(other.gameObject.tag == ("DestroyCarrosNivel2"))
+        {
+            Destroy(gameObject);
+        }
+
+        if(other.gameObject.tag == ("BoxCollider3"))
+        {
+            source.clip = carHorn[Random.Range(0, carHorn.Length)];
+            source.Play();
+            
+        }
+
+
+
+
+
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //MueveObstaculo();
+        //if (timemanager.TimeIsStopped)
+        //{
+            //GetComponent<Rigidbody>().position += Vector3.back * zerovelocity * Time.deltaTime;
+
+        //}
+        //else
+        {
+            GetComponent<Rigidbody>().position += Vector3.right * velocity * Time.deltaTime;
+        }
+        
+    }
+}
